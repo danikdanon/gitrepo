ce594a0380e7e67790161cec8852072ac0561437
danikdanon <daniilturkenich@mail.ru>
2018-02-20 08:26:33 +0300

    Initial Commit

---
+++ git project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  git project
+//
+//  Created by Даниил on 20.02.2018.
+//  Copyright © 2018 Даниил. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
